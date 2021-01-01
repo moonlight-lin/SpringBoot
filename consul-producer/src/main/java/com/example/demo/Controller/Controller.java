081da9bfa38eb81f258bec1bc4ea6dd07fb3330f
moonlight-lin <moonlight-lin@users.noreply.github.com>
2021-01-02 01:49:00 +0800

    Consul - discovery & load balance

--- consul-producer/src/main/java/com/example/demo/Controller/Controller.java
+++ consul-producer/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,10 @@ public class Controller {
     @RequestMapping("/hello")
     public String hello() {
         return "consul-service-1 instance-1";
+    }
+    
+    @GetMapping("/product/{id}")
+    public String product(@PathVariable("id") String id) {
+        return "price of product " + id + " is 100";
     }
 }
